adac01e6748180a2789422874441ee66acaefc2b
mikoto2000 <mikoto2000@gmail.com>
2025-05-27 08:01:41 +0000

    DB スキーマ定義間違いを修正。

--- data/postgres/initdb.d/001.schema.sql
+++ data/postgres/initdb.d/001.schema.sql
@@ -1,9 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 drop table if exists account;
 
 create table account (
   id uuid primary key default uuid_generate_v4(),
   issuer text not null,
-  sub text not null
+  sub text not null,
   unique (issuer, sub)
 );
 
