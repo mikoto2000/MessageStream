44045aef4604f39e235c9fc1fdf3e8fdfeca8600
mikoto2000 <mikoto2000@gmail.com>
2025-05-30 12:19:09 +0000

    OIDC によるログイン機能を追加。

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import { useAuth } from "react-oidc-context";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const auth = useAuth();
+
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+  if (!auth.isAuthenticated) {
+    return <button onClick={() => {
+      // ログイン後に元居たページに戻れるように、
+      // 今いるパスをセッションストレージに記憶しておく
+      sessionStorage.setItem('path', location.pathname);
+
+      // state の削除
+      auth.clearStaleState();
+
+      // 認可サーバーにアクセスしてログイン、
+      // ログインに成功したら redirect_uri にリダイレクト
+      auth.signinRedirect();
+    }}>Log in</button>
+  }
+
+  if (auth.isLoading || auth.error) {
+      return <>認証中</>;
+  }
+
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+      return <div>Signing you in...</div>;
+    case "signoutRedirect":
+      return <div>Signing you out...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Oops... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { AuthProvider } from 'react-oidc-context'
+
 import './index.css'
 import App from './App.tsx'
 
+const oidcConfig = {
+    authority:"http://keycloak:8080/realms/myrealm",
+    client_id: "myrealm-id",
+    client_secret: "myrealm-secret",
+    redirect_uri: "http://localhost:5173",
+    onSigninCallback: () => {
+        // セッションストレージから元居たパスを取得セッションストレージ内のデータは削除
+        const redirectLocation = sessionStorage.getItem('path');
+        sessionStorage.removeItem('path');
+
+        if (redirectLocation) {
+            // ローカルストレージにパスがあったらそこにリダイレクト
+            window.location.replace(redirectLocation);
+        } else {
+            // クエリパラメータに色々な情報が載っているので、それを削除する
+            window.history.replaceState(
+                {},
+                document.title,
+                window.location.pathname
+            );
+        }
+    }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...oidcConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
